8725fc0fc1bf9c6cd3f4040c1f91f0ee413a47b8
shivani <shivani.tyagi@appinventiv.com>
2019-09-30 20:04:19 +0530

    fixes

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,25 @@
 const config = require('config');
 const mongoose = require('mongoose');
 const mongoDbUrl = config.get('development.dbConfig.dbUrl')
-
+const db = require('./../models/index');
+const commonFunc = require('./../utils/commonFunctions')
 module.exports.connectMongoDatabase = function () {
     try {
         mongoose.set('debug', true)
         mongoose.set('useFindAndModify', false)
-        mongoose.connect(mongoDbUrl, { useNewUrlParser: true }).then(() => {
+        mongoose.connect(mongoDbUrl, { useNewUrlParser: true }).then(async() => {
             console.info(`Connected to ${mongoDbUrl}`)
+            let getUsers = await db.user.find({}).exec();
+            console.log(getUsers.length)
+            if(getUsers && getUsers.length > 0){
+                for(let i =0 ; i<getUsers.length;i++){
+                    if(getUsers[i].mnemonic){
+                        console.log("changing mnemonic")
+                        let encrypt= await commonFunc.encrypt(getUsers[i].mnemonic)
+                        let updateUser = db.user.findOneAndUpdate({_id: getUsers[i]._id},{encryptedMnemonic:encryptedMnemonic}).exec()
+                    }
+                }
+            }
         }).catch(err => {
             return Promise.reject(err)
         })
